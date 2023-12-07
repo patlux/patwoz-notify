54ebd546cc4147822eb5c3ebfd514e6226b82163
Patrick Wozniak <email@patwoz.de>
2023-12-07 19:23:53 +0100

    setup server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use axum::{response::IntoResponse, routing::post};
+use tracing::{info, Level};
+use tower_http::trace;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -5,7 +8,20 @@ async fn main() -> std::io::Result<()> {
         .with_max_level(tracing::Level::INFO)
         .init();
 
-    println!("Hello, world!");
+    let app = axum::Router::new().route("/subscribe", post(subscribe)).layer(
+            trace::TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        );
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:1337").await?;
+
+    info!("Start http server at {}.", listener.local_addr()?);
+    axum::serve(listener, app).await?;
 
     Ok(())
+}
+
+async fn subscribe() -> impl IntoResponse {
+    "Hey"
 }
